d8bef5411537f1ef3c122c515499e3c0fbbf6338
Sean McCarthy <spm64@pitt.edu>
2016-04-17 22:31:22 -0400

    getHighBid test GREEN

--- DataPoint.java
+++ DataPoint.java
@@ -1,12 +1,18 @@
 public class DataPoint {
     double openBid;
+    double highBid;
 
 
     public DataPoint(double[] rawData) {
         this.openBid = rawData[0];
+        this.highBid = rawData[1];
     }
 
     public double getOpenBid(){
         return this.openBid;
+    }
+
+    public double getHighBid() {
+        return this.highBid;
     }
 }
