42b8a075b9ce0c767660096790a5514d416de7a7
Sean McCarthy <spm64@pitt.edu>
2016-04-17 22:45:46 -0400

    getOpenAskTest GREEN

--- DataPoint.java
+++ DataPoint.java
@@ -3,13 +3,15 @@ public class DataPoint {
     double highBid;
     double lowBid;
     double closeBid;
-
+    
+    double openAsk;
 
     public DataPoint(double[] rawData) {
         this.openBid = rawData[0];
         this.highBid = rawData[1];
         this.lowBid = rawData[2];
         this.closeBid = rawData[3];
+        this.openAsk = rawData[4];
     }
 
     public double getOpenBid(){
@@ -26,5 +28,9 @@ public class DataPoint {
 
     public double getCloseBid() {
         return closeBid;
+    }
+
+    public double getOpenAsk() {
+        return openAsk;
     }
 }
