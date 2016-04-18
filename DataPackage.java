e70d0806bde770d8135ba802093582c7a2545623
Sean McCarthy <spm64@pitt.edu>
2016-04-18 10:17:16 -0400

    dataPackageMultiPointConstruction Test

--- DataPackage.java
+++ DataPackage.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DataPackage {
     private ArrayList<DataPoint> dataPoints;
@@ -7,6 +8,11 @@ public class DataPackage {
     public DataPackage(){
         dataPoints = new ArrayList<DataPoint>();
         numDataPoints = 0;
+    }
+
+    public DataPackage(DataPoint[] dataPointsToAdd){
+        dataPoints = new ArrayList<DataPoint>(Arrays.asList(dataPointsToAdd));
+        numDataPoints = dataPointsToAdd.length;
     }
 
     public void addDataPoint(DataPoint dataPoint){
